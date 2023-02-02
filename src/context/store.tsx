adab215a3e40ea3c120e0c1e5127212456b8fc5c
atousa <atoosa.havaseli@gmail.com>
2023-02-02 14:26:08 +0330

    Add Context.

---
+++ src/context/store.tsx
@@ -0,0 +1,19 @@
+import React, { createContext, useReducer } from "react";
+
+// const AppContext = createContext({})
+// const ContactReducer = (state, action) => {
+//     console.log(state, action);
+//     return state
+// }
+// const InitialState = {
+//     Contacts: {
+//         List: []
+//     }
+// }
+// interface AppContextProviderProps extends React.PropsWithChildren { }
+// const AppContextProvider: React.FunctionComponent<AppContextProviderProps> = ({ children }): JSX.Element => {
+//     const [state, dispatch] = useReducer(ContactReducer, InitialState)
+//     return <AppContext.Provider value={ }>{children}</AppContext.Provider>
+// }
+
+// export { AppContext, AppContextProvider }
