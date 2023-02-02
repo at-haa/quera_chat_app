8114db9307ad2f87ab7caeb8628be17aaddf3c25
atousa <atoosa.havaseli@gmail.com>
2023-02-02 14:31:49 +0330

    Add Context.

--- src/context/store.tsx
+++ src/context/store.tsx
@@ -1,19 +1,20 @@
 import React, { createContext, useReducer } from "react";
 
-// const AppContext = createContext({})
-// const ContactReducer = (state, action) => {
-//     console.log(state, action);
-//     return state
-// }
-// const InitialState = {
-//     Contacts: {
-//         List: []
-//     }
-// }
-// interface AppContextProviderProps extends React.PropsWithChildren { }
-// const AppContextProvider: React.FunctionComponent<AppContextProviderProps> = ({ children }): JSX.Element => {
-//     const [state, dispatch] = useReducer(ContactReducer, InitialState)
-//     return <AppContext.Provider value={ }>{children}</AppContext.Provider>
-// }
+const AppContext = createContext({})
+const ContactReducer = (state: any, action: any) => {
+    console.log(state, action);
+    return state
+}
+const InitialState = {
+    Contacts: {
+        List: []
+    }
+}
+interface AppContextProviderProps extends React.PropsWithChildren { }
+const AppContextProvider: React.FunctionComponent<AppContextProviderProps> = ({ children }): JSX.Element => {
+    const [state, dispatch] = useReducer(ContactReducer, InitialState)
+    // return <AppContext.Provider value={ }>{children}</AppContext.Provider>
+    return <div></div>
+}
 
-// export { AppContext, AppContextProvider }
+export { AppContext, AppContextProvider }
